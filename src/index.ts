606f044a1e937ba50d58609276dccc21dc9aaa4a
NamPE286 <nambuihung654@gmail.com>
2025-01-09 21:42:47 +0700

    rename routes to routers

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const PORT = process.env.PORT;
 
 app.use(express.json());
 
-app.use('/product', require('./routes/product').default)
+app.use('/product', require('./routers/product').default)
 
 app.listen(PORT, () => {
     console.log(`Server started on ${PORT}`);
